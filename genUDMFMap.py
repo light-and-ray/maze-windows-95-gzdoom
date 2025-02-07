efc3f0936b92ea39cce81acafc9cc4c590f7c841
Andray <light.and.ray@gmail.com>
2025-02-07 21:47:21 +0400

    first commit

---
+++ genUDMFMap.py
@@ -0,0 +1,88 @@
+
+from dataclasses import dataclass
+import omg
+
+@dataclass
+class Line:
+    v1: tuple[int]
+    v2: tuple[int]
+
+MAZE_W = 10
+TEXTURE_W = 33
+
+def save(lines: list[list[Line]], box: list[Line]):
+    vertexes : list[tuple[int]] = []
+    def addVertex(newVertex) -> int:
+        for i in range(len(vertexes)):
+            if vertexes[i] == newVertex:
+                return i
+        vertexes.append(newVertex)
+        return len(vertexes) - 1
+
+    allLines : list[Line] = []
+    for lineRow in lines:
+        allLines.extend(lineRow)
+
+    udmfMap = omg.UMapEditor()
+    udmfMap.namespace = "zdoom"
+
+    sideIdx = 0
+    for line in allLines:
+        v1Idx = addVertex(line.v1)
+        v2Idx = addVertex(line.v2)
+        udmfMap.linedefs.append(omg.ULinedef(v1=v1Idx, v2=v2Idx, sidefront=sideIdx, sideback=sideIdx+1))
+        udmfMap.linedefs[-1].twosided = True
+        sideIdx += 2
+    sideIdx -= 1
+    for line in box:
+        v1Idx = addVertex(line.v1)
+        v2Idx = addVertex(line.v2)
+        udmfMap.linedefs.append(omg.ULinedef(v1=v1Idx, v2=v2Idx, sidefront=sideIdx))
+        sideIdx += 1
+    # sideIdx -= 1
+    for _ in range(sideIdx):
+        udmfMap.sidedefs.append(omg.USidedef(sector=0))
+        udmfMap.sidedefs[-1].texturemiddle = "None"
+
+    udmfMap.sectors.append(omg.USector(textureceiling="None", texturefloor="None",
+                            heightfloor=0, heightceiling=TEXTURE_W, lightlevel=200))
+    udmfMap.things.append(omg.UThing(x=0, y=0, ednum=1))
+    udmfMap.vertexes = [omg.UVertex(*v) for v in vertexes]
+
+    mapName = 'maze95'
+    wad = omg.WAD()
+    wad.udmfmaps[mapName] = udmfMap.to_lumps()
+    wadPath = mapName + ".wad"
+    wad.to_file(wadPath)
+
+
+def genUDMFMap():
+    lines : list[list[Line]] = []
+    num = 0
+    for i in range(MAZE_W+1):
+        lines.append([])
+        for j in range(MAZE_W):
+            lines[i].append(Line(v1=(i*TEXTURE_W, j*TEXTURE_W), v2=(i*TEXTURE_W, (j+1)*TEXTURE_W)))
+            num += 1
+    for i in range(MAZE_W):
+        lines.append([])
+        for j in range(MAZE_W+1):
+            lines[i].append(Line(v1=(i*TEXTURE_W, j*TEXTURE_W), v2=((i+1)*TEXTURE_W, j*TEXTURE_W)))
+            num += 1
+
+    box : list[Line] = []
+    A = (-TEXTURE_W, MAZE_W*TEXTURE_W+TEXTURE_W)
+    B = (MAZE_W*TEXTURE_W+TEXTURE_W, MAZE_W*TEXTURE_W+TEXTURE_W)
+    C = (MAZE_W*TEXTURE_W+TEXTURE_W, -TEXTURE_W)
+    D = (-TEXTURE_W, -TEXTURE_W)
+
+    box.append(Line(A, B))
+    box.append(Line(B, C))
+    box.append(Line(C, D))
+    box.append(Line(D, A))
+
+    save(lines, box)
+
+
+if __name__ == "__main__":
+    genUDMFMap()
---
+++ launch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gzdoom -iwad DOOM.WAD -file maze95.wad +map maze95
